755e9d1258fd9d365520455821e57364c4ea0c32
Martino Vallara <martino.vallara@xpeppers.com>
2020-09-05 12:26:56 +0200

    parse parameters repositorypath and messageFilter

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const gitlog = require("gitlog").default;
 
 const options = {
-    repo: __dirname + "/gitRepoPhoenix",
+    repo: "",
     number: 5000,
     fields: ["subject"],
     after: "2020-07-03",
     execOptions: { maxBuffer: 10000 * 1024 },
 };
 
+console.log(process.argv);
+options.repo = process.argv[2];
+
+
 // Synchronous
 const commits = gitlog(options);
+const messageFilter = process.argv[3];
+
 var cardIdDistinct = [];
-var phCommits = commits.filter(commit => commit.subject.includes("PH ["));
+var phCommits = commits.filter(commit => commit.subject.includes(messageFilter));
 const grepId = /\[(\d*)\]/;
 phCommits.reduce((cardIdList, commit) => {
     const parseId = grepId.exec(commit.subject);
